568d62e7e0eaffaf152bb4cac8efc984f52f1215
valdir-ti <valdir.ti@gmail.com>
2020-06-29 23:18:23 -0300

    Construção da primeira página da aplicação

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
+import Home from './pages/Home';
 
 function App() {
-  const [counter, setCounter] = useState(1);//[valor do estado, funcção que atualiza o valor do estado]
-
-  function handleButtonClick(){
-    setCounter(counter + 1);
-    console.log(counter);
-  }
-  
-  function handleMinusClick(){
-    setCounter(counter - 1);
-    console.log(counter);
-  }
-
   return (
-    <div>      
-      <Header title="Header da Aplicação"/>
-      
-      <h1>{counter}</h1>
-
-      <button type="button" onClick={handleButtonClick}>Aumentar</button>
-      <button type="button" onClick={handleMinusClick}>Diminuir</button>
-
-    </div>
+    <Home />
   )
 }
 
